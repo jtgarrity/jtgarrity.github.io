ee09f6665b5bea5e6d184bff218d5b4bcf665ca9
Hovsep Agop <hka245@nyu.edu>
2016-10-23 18:12:08 -0700

    updates 102316

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('a[href="#how-it-works"]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 700);
+        return false;
+      }
+    }
+  });
+});
